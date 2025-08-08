cd55bd68927d17495ec64ce607825d21344c2667
codeyoursite <wilson_macmahon@outlook.com>
2025-08-08 14:32:30 +0100

    New logo, Nav bar

---
+++ nav.js
@@ -0,0 +1,12 @@
+var x = document.createElement("div");
+x.setAttribute("id", "header");
+x.innerHTML = `<nav class="navbar">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white" href="https://code-cadets.getlearnworlds.com">
+      <img src="https://codeyoursite.github.io/codecadets/logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      Code Cadets
+    </a>
+  </div>
+</nav>`
+document.body.appendChild(x);
+
